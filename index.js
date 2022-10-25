c8e15b9ebfa2dc62784696baafb013008bf1d3b9
egordiets <egordiets@yandex.ru>
2022-10-25 20:44:31 +0300

    hw-1

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-//import colors from 'colors';
-const colors = require('colors')
+import colors from 'colors';
 
-//const a = prompt('Введите простое число');
-const b = 2;
+let num1 = parseInt(prompt('Введите первое число'));
+let num2 = parseInt(prompt('Введите второе число'));
+let a = prompt('Введите аргумент');
+if(!Number.isInteger(a)){
+    throw new Error();
+}
 
-if ( Number.isInteger(b) && b>1 && b<998){
-    // while(a>0){
-    //     a
-    // }
-    for(let i=0; i<3; i++){
-        c = b%10;
-        b = b/10;
-        const units = 0;
-        if ( b<0 ){
-            console.log(colors.green.c)
-        } else{
-            units = colors.green.c;
-        }
+if ( Number.isInteger(a) && a>num1 && a<num2){
+    if( a<10 ){
+        console.log(colors.green(a))
+    } else if( a> 9 && a<100 ){
+        let b = a%10;
+        a = Math.round(a/10);
+        console.log(colors.yellow(a), colors.green(b));
+    } else if(a>100){
+        let b = a%10;
+        a = Math.round(a/10);
+        let c = a%10;
+        a = Math.round(a/10);
+        console.log(colors.red(a), colors.yellow(c), colors.green(b))
     }
+} else {
+    console.log('Простых чисел в диапазоне нет!')
 }
